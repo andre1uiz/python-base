01e8373e33592921846cb3f7cfef30859965f435
Andre Luiz Goncalves <andre1uiz@hotmail.com>
2024-10-22 19:20:54 +0000

    visto exercicio escola com listas e sets \ iniciado estudo dicionario

---
+++ cadastro_de_produtos.py
@@ -0,0 +1,15 @@
+#!/app/python-base/.venv/bin/python
+"""Cadastro de produto
+
+"""
+__version__ = "0.1.0"
+
+produto_nome = "Caneta"
+produto_cor1  = "azul"
+produto_cor2 = "Branco"
+produto_preco = 3.23
+produto_dimensao_altura = 12.1
+produto_dimensao_largura = 0.8
+produto_em_estoque = True
+produto_codigo = 45678
+produto_codebar = None
