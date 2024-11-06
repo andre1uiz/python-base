d5211795d2463a016c097afa1a8c13f9a6bcdfeb
Andre Luiz Goncalves <andre1uiz@hotmail.com>
2024-11-06 12:51:41 +0000

    Erro: EAFP try, except

--- errors.py
+++ errors.py
@@ -1,20 +1,23 @@
 #!/app/python-base/.venv/bin/python
 """
+tratamento de erros precisa ser especificos
 """
 import os
 import sys
 
 # Look before you leap (LBYP)
+#EAFP Easy to ask forgiveness than permission
 
-if os.path.exists("names.txt"):
-    #Race condition
-    input("...")
-    names = open("names.txt").readlines()
-else:
-    print("[Error file names.txt not found]")
+try:
+    names = open("names.txt").readlines() #FileNotFoundError
+    1/0 #ZeroDivisionError
+    print(names.banana) #AtributeError
+#except: #Bare except - irá para esta rotina em todos os erros possiveis
+except FileNotFoundError as e:
+    print(f"[Error] {str(e)}.")
     sys.exit(1)
-
-if len(names) >= 3:
+try:
     print(names[2])
-else:
+except:
     print("[Error] Missing name in the list")
+    #TODO: usar retry
